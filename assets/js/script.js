62cb2ff98b03c6a72d39b4e177bdd2b9a83718a1
ZabeenaSherzoie <zabeenasherzoie@gmail.com>
2022-09-22 05:03:47 +0000

    adding the check answer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,79 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     let buttons = this.getElementsByTagName('button');
-    for (let button of buttons){
-        button.addEventListener('click', function(){
-            if (this.getAttribute('data-type') === 'submit'){
-                alert("you clicked submit");
-            }else{
-                let gameType =this.getAttribute('data-type');
+    for (let button of buttons) {
+        button.addEventListener('click', function () {
+            if (this.getAttribute('data-type') === 'submit') {
+                checkAnswer();
+            } else {
+                let gameType = this.getAttribute('data-type');
                 runGame(gameType);
             }
         })
-        
+
     }
     runGame('addition')
 })
 
-function runGame(gameType){
-    let num1 = Math.floor(Math.random()*25)+1;
-    let num2 = Math.floor(Math.random()*25)+1;
-    if (gameType ==='addition'){
-        displayAdditionQuestion(num1,num2);
-    }else if (gameType ==='multiply'){
-        displayMultiplyQuestion(num1,num2)
-    }else{
+function runGame(gameType) {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else if (gameType === 'multiply') {
+        displayMultiplyQuestion(num1, num2)
+    } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`
     }
 }
-function checkAnswer(){
+
+function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
 
 }
-function calculateCorrectAnswer(){
+
+function calculateCorrectAnswer() {
+    let opernad1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+    if (operator==="+"){
+        return [opernad1 + operand2,'addition']
+    }else{
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game: ${gameType}. Aborting!`
+    }
+}
+
+function incrementScore() {
 
 }
-function incrementScore(){
+
+function incrementWrongAnswer() {
 
 }
-function incrementWrongAnswer(){
-    
-}
-function displayAdditionQuestion(operand1,operand2){
+
+function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
 
 }
-function displaySubtractQuestion(){
-    
+
+function displaySubtractQuestion() {
+
 }
-function displayMultiplyQuestion(operand1,operand2){
+
+function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "*";
